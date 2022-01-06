dbf217e08d2d5c2e48a729f0a275200bbc828dde
karthikeyang <karthikeyang@GCDTP050.GADAGET.COM>
2022-01-06 20:26:32 +0530

    Remove other codes

--- GitDemoProject/src/TestSample1.java
+++ GitDemoProject/src/TestSample1.java
@@ -4,16 +4,7 @@ public class TestSample1 {
 	public static void main(String[] args) {
 	
 		System.out.println("This is the TestSample1 class");
-		int c = 10;
-		if(c%2 == 0) {
-			System.out.println("C is even");
-		}else {
-			System.out.println("C is odd");
-		}
 		
-		int a = 10;
-		int b = a + c;
-		System.out.println("b is "+ b);
 	}
 
 }
