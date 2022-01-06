693e54254ace08a2b3518158265fbcf71a6262a0
karthikeyang <karthikeyang@GCDTP050.GADAGET.COM>
2022-01-06 20:22:11 +0530

    Project Start

--- GitDemoProject/src/TestSample1.java
+++ GitDemoProject/src/TestSample1.java
@@ -1,8 +1,8 @@
-
+package src;
 public class TestSample1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		System.out.println("This is the TestSample1 class");
 		int c = 10;
 		if(c%2 == 0) {
--- GitDemoProject/src/TestSample_2.java
+++ GitDemoProject/src/TestSample_2.java
@@ -1,8 +1,8 @@
-
+package src;
 public class TestSample_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("This is the TestSample_2 class..");
 	}
 
